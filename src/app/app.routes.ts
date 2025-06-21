0d055a53aa93155f787d7c5a5e0e4fd224176057
Docente-Lab <davidserrudoclases@gmail.com>
2025-06-20 20:59:03 -0400

    primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AcercaDeComponent } from './pages/acerca-de/acerca-de.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'inicio',
+        component:InicioComponent
+    },
+    {
+        path:'acercade',
+        component:AcercaDeComponent
+    },
+    {
+        path:'**',
+        component:InicioComponent
+    }
+];
